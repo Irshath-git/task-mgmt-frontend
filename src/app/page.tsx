aa00ad634677a56adc226edfde0e0ea39c1f9c6a
Irshath-git <112928192+Irshath-git@users.noreply.github.com>
2024-08-01 22:32:23 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,6 @@ import type { AppProps } from 'next/app';
 
 import PagePlaceholder from '@/components/page-placeholder';
 
-export default function Home({ Component, pageProps }: AppProps) {
+export function Home({ Component, pageProps }: AppProps) {
   return <PagePlaceholder pageName="Home" {...pageProps} />;
 }
